17f75501d8f230706c6a02225033d1a260ade5f6
Yogi Pradnyana <15tkj1yogipradnyana.com>
2022-03-18 23:31:26 +0800

    Menambahkan function removeBtn(), menambahkan event onclick pada [x] dan menambahkan #todo pada css

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const todo = document.getElementById('todo');
 // Fitur Todo
 // 1. Menambahkan V
-// 2. Mencoret kalau sudah selesai
+// 2. Mencoret kalau sudah selesai V
 // 3. Menghapus
 
 function add() {
   // 1. mengambil nilai dari input
   let newText = document.getElementById('new_text');
   // 2. menambahkan li baru atau list todo baru
-  let newTodo = '<li><span onclick="beriGaris(this)">' + newText.value + '</span></li>';
+  let newTodo = '<li><span onclick="beriGaris(this)">' + newText.value + '</span>' + '<span onclick="removeBtn(this)">[x]</span></li>';
   todo.insertAdjacentHTML('afterbegin', newTodo);
   // 3. menghapus fieldnya
   newText.value = '';
@@ -16,4 +16,8 @@ function add() {
 
 function beriGaris(el) {
   el.classList.toggle('done');
+}
+
+function removeBtn(a) {
+  a.parentElement.remove();
 }
